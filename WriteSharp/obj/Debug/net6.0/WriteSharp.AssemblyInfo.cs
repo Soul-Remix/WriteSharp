4923687d7b3ea86ba3de353a633ef8ae2ebd822c
Soul-Remix <soulremix-dev@pm.me>
2022-12-22 20:30:15 +0300

    Expose internal classes to testing project

--- WriteSharp/obj/Debug/net6.0/WriteSharp.AssemblyInfo.cs
+++ WriteSharp/obj/Debug/net6.0/WriteSharp.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("WriteSharp.Tests")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WriteSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
