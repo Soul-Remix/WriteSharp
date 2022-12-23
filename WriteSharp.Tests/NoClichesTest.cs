ae098be9a9aac7ac01581d4721b020dc530abb08
Soul-Remix <soulremix-dev@pm.me>
2022-12-23 11:53:28 +0300

    Write unit tests for NoCliches

---
+++ WriteSharp.Tests/NoClichesTest.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using WriteSharp.Checks;
+using Xunit;
+
+namespace WriteSharp.Test;
+
+public class NoClichesTest
+{
+    private readonly NoCliches _noCliches;
+
+    private const string ClichesInSentence = "Writing specs puts me at loose ends.";
+    private const string ClichesInSentenceWithFormatting = "Writing specs puts me at   loose\n ends.";
+    private const string GoodSentence = "The good dog jumps over the bad cat.";
+
+    public NoClichesTest()
+    {
+        _noCliches = new NoCliches();
+    }
+
+    [Fact]
+    public void NoCliches_SentenceFilledWithCliche_NotEscapeNotice()
+    {
+        List<CheckResult> results = _noCliches.Check(ClichesInSentence);
+        List<CheckResult> expected = new List<CheckResult>()
+        {
+            new()
+            {
+                Index = 22,
+                Offset = 13,
+                Reason = "\"at loose ends\" is a cliche"
+            }
+        };
+        Assert.Equal(expected,results);
+    }
+    
+    [Fact]
+    public void NoCliches_SentenceFilledWithCliche_NoProblemWhiteFormatting()
+    {
+        List<CheckResult> results = _noCliches.Check(ClichesInSentenceWithFormatting);
+        List<CheckResult> expected = new List<CheckResult>()
+        {
+            new()
+            {
+                Index = 22,
+                Offset = 16,
+                Reason = "\"at   loose\n ends\" is a cliche"
+            }
+        };
+        Assert.Equal(expected,results);
+    }
+}
