65abe7c06c7aac919a6087fe6adf9f5b50861091
Soul-Remix <soulremix-dev@pm.me>
2022-12-24 17:00:40 +0300

    Write unit tests StartsWithSo

---
+++ WriteSharp.Tests/StartsWithSoTest.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using WriteSharp.Checks;
+using Xunit;
+
+namespace WriteSharp.Test;
+
+public class StartWithSoTest
+{
+    private readonly StartWithSo _startWithSo;
+    private const string SentenceStartWithSo = "So, I saw the cute puppy and I picked it up. ";
+    private const string GoodSentence = "The puppy was cute, so I picked it up.";
+
+    public StartWithSoTest()
+    {
+        _startWithSo = new StartWithSo();
+    }
+
+    [Fact]
+    public void StartWithSo_SentenceStartStartWithSo()
+    {
+        List<CheckResult> results = _startWithSo.Check(SentenceStartWithSo);
+        List<CheckResult> expected = new List<CheckResult>()
+        {
+            new()
+            {
+                Index = 0,
+                Offset = 2,
+                Reason = "\"So\" adds no meaning"
+            }
+        };
+        
+        Assert.Equal(expected, results);
+    }
+
+    [Fact]
+    public void StartWithSo_GoodSentence()
+    {
+        List<CheckResult> results = _startWithSo.Check(GoodSentence);
+
+        Assert.Empty(results);
+    }
+}
