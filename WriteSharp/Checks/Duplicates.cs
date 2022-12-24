d459c56ff54f1ae646089fbb491892a7e414cf9d
Soul-Remix <soulremix-dev@pm.me>
2022-12-24 17:01:17 +0300

    Complete Duplicates

---
+++ WriteSharp/Checks/Duplicates.cs
@@ -0,0 +1,32 @@
+using System.Text.RegularExpressions;
+using WriteSharp.Interfaces;
+
+namespace WriteSharp.Checks;
+
+public class Duplicates : IChecker
+{
+    private readonly Regex _regex;
+
+    public Duplicates()
+    {
+        _regex = new Regex(@"\b(\w+?)\s\1\b", RegexOptions.IgnoreCase);
+    }
+
+    public List<CheckResult> Check(string text)
+    {
+        List<CheckResult> results = new List<CheckResult>();
+
+        foreach (Match match in _regex.Matches(text))
+        {
+            CheckResult result = new CheckResult()
+            {
+                Index = match.Index,
+                Offset = match.Length,
+                Reason = $"\"{match.Value}\" is a duplicate"
+            };
+            results.Add(result);
+        }
+
+        return results;
+    }
+}
