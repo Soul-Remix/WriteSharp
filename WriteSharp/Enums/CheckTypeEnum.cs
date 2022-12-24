e7bdfed1d5c4eff949fa511cd805e1286e4390d2
Soul-Remix <soulremix-dev@pm.me>
2022-12-24 17:59:36 +0300

    Add an enum for check type

---
+++ WriteSharp/Enums/CheckTypeEnum.cs
@@ -0,0 +1,14 @@
+namespace WriteSharp.Enums;
+
+public enum CheckTypeEnum
+{
+    AdverbWhere,
+    Duplicates,
+    EPrime,
+    NoCliches,
+    PassiveVoice,
+    StartWithSo,
+    ThereIs,
+    TooWordy,
+    WeaselWords,
+}
