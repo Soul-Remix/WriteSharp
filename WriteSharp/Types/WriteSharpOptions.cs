c6712c5f2b5e01a5acce5c55662b6bef116907f8
Soul-Remix <soulremix-dev@pm.me>
2022-12-24 18:00:29 +0300

    Add a class for WriteSharp Options

---
+++ WriteSharp/Types/WriteSharpOptions.cs
@@ -0,0 +1,95 @@
+using WriteSharp.Enums;
+
+namespace WriteSharp.Types;
+
+public class WriteSharpOptions
+{
+    private readonly Dictionary<CheckTypeEnum, bool> _checks;
+
+    public bool AdverbWhere
+    {
+        get => _checks[CheckTypeEnum.AdverbWhere];
+        set => _checks[CheckTypeEnum.AdverbWhere] = value;
+    }
+
+    public bool Duplicates
+    {
+        get => _checks[CheckTypeEnum.Duplicates];
+        set => _checks[CheckTypeEnum.Duplicates] = value;
+    }
+
+    public bool EPrime
+    {
+        get => _checks[CheckTypeEnum.EPrime];
+        set => _checks[CheckTypeEnum.EPrime] = value;
+    }
+
+    public bool NoCliches
+    {
+        get => _checks[CheckTypeEnum.NoCliches];
+        set => _checks[CheckTypeEnum.NoCliches] = value;
+    }
+
+    public bool PassiveVoice
+    {
+        get => _checks[CheckTypeEnum.PassiveVoice];
+        set => _checks[CheckTypeEnum.PassiveVoice] = value;
+    }
+
+    public bool StartWithSo
+    {
+        get => _checks[CheckTypeEnum.StartWithSo];
+        set => _checks[CheckTypeEnum.StartWithSo] = value;
+    }
+
+    public bool ThereIs
+    {
+        get => _checks[CheckTypeEnum.ThereIs];
+        set => _checks[CheckTypeEnum.ThereIs] = value;
+    }
+
+    public bool TooWordy
+    {
+        get => _checks[CheckTypeEnum.TooWordy];
+        set => _checks[CheckTypeEnum.TooWordy] = value;
+    }
+
+    public bool WeaselWords
+    {
+        get => _checks[CheckTypeEnum.WeaselWords];
+        set => _checks[CheckTypeEnum.WeaselWords] = value;
+    }
+
+    public List<String> WhiteList { get; set; } = new List<string>();
+
+    public WriteSharpOptions()
+    {
+        _checks = new Dictionary<CheckTypeEnum, bool>()
+        {
+            { CheckTypeEnum.AdverbWhere, true },
+            { CheckTypeEnum.Duplicates, true },
+            { CheckTypeEnum.EPrime, true },
+            { CheckTypeEnum.NoCliches, true },
+            { CheckTypeEnum.PassiveVoice, true },
+            { CheckTypeEnum.StartWithSo, true },
+            { CheckTypeEnum.ThereIs, true },
+            { CheckTypeEnum.TooWordy, true },
+            { CheckTypeEnum.WeaselWords, true },
+        };
+    }
+
+    public void AddToWhiteLIst(string word)
+    {
+        WhiteList.Add(word);
+    }
+
+    public void RemoveFromWhiteList(string word)
+    {
+        WhiteList.Remove(word);
+    }
+
+    internal Dictionary<CheckTypeEnum, bool> GetChecks()
+    {
+        return _checks;
+    }
+}
