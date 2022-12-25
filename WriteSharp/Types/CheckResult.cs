e9c919cda133ed2c49afbd011f7a7eb95c6291e3
Soul-Remix <soulremix-dev@pm.me>
2022-12-25 23:28:37 +0300

    Change CheckResult to class and override Equals and GetHashCode methods

--- WriteSharp/Types/CheckResult.cs
+++ WriteSharp/Types/CheckResult.cs
@@ -1,8 +1,28 @@
 namespace WriteSharp.Types;
 
-public record CheckResult
+public class CheckResult
 {
     public String Reason { get; set; } = "";
     public int Index { get; set; }
     public int Offset { get; set; }
+
+    public override string ToString()
+    {
+        return $"Reason = {Reason}, Index = {Index}, Offset = {Offset}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        return this.ToString().Equals(obj.ToString());
+    }
+
+    public override int GetHashCode()
+    {
+        return this.ToString().GetHashCode();
+    }
 }
